da17630a5c5a363a4854d1e17537225b316dd9f1
rohit-s-init <rohitrameshwarsawant@gmail.com>
2026-02-28 22:21:53 +0530

    changing notific css naming

--- src/pages/Notifications.jsx
+++ src/pages/Notifications.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from "./notifications.module.css";
+import styles from "./Notifications.module.css";
 
 function Notification() {
   const [notifications, setNotifications] = useState([
